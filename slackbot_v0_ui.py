a7b2e22d6f74deb999d22a406564412203a19b2b
Nihan Pol <33101514+NihanPol@users.noreply.github.com>
2021-10-14 16:45:34 -0500

    remove dotenv use

--- slackbot_v0_ui.py
+++ slackbot_v0_ui.py
@@ -5,7 +5,7 @@ from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
-load_dotenv(dotenv_path = env_path)
+#load_dotenv(dotenv_path = env_path)
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
